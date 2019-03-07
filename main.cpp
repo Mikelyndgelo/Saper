43c672dba0e649043392032f6732034b4ab2216a
Kvaskov Max <kvmaxim55@gmail.com>
2019-03-07 16:36:40 +0300

    //First version

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include<QtWidgets>
+#include"counter.h"
+#include"button.h"
+#include "test.h"
+#include<ctime>
+
+//Написать несколько кнопок, и задать обработку каждой из них))))
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    srand(time(NULL));
+
+    Button button;
+    button.show();
+
+
+    return app.exec();
+}
+
+
+//C:/Programming/Qt projects/Saper/images.jpg
+
+
+/*QLabel lbl("0");                //Надпись
+QPushButton cmd("ADD");         //Нажимающая кнопка
+Counter counter;                //Объект счетчик
+
+lbl.show();
+cmd.show();
+
+//Сука всегда проверяй синтаксис ,не забывай про скобки
+QObject::connect(&cmd,SIGNAL(clicked()),            //Нажатие кнопки вызывает метод slotInc класса count, который увеличтвает значение цифры
+                 &counter, SLOT(slotInc())
+                 );
+
+QObject::connect(&counter,SIGNAL(counterChanged(int)),      //Этот метод (counterChanged(int)) увеличивает собственно значение цифры
+                 &lbl, SLOT(setNum(int))
+                 );
+
+QObject::connect(&counter,SIGNAL(goodbay()),                //Когда значение 5 раз кликаем вызывается метод slotInc() а в нем нам передается метод goodbay()
+                 &app, SLOT(quit())
+                 );*/
+
+//Слот то такая же функция по сути которая вызывается при обработке события
+
+
+
+
+
+
+
